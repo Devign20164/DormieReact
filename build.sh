36f457e68ea2420f38b9b4ce446b7964e61acd2d
Devign20164 <romeyluigi20164@gmail.com>
2025-04-23 14:59:36 +0800

    bread

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install dependencies for main project
-npm install
+npm install --legacy-peer-deps
 
 # Install backend dependencies
 cd backend
-npm install
+npm install --legacy-peer-deps
 cd ..
 
 # Install frontend dependencies and build
 cd frontend
-npm install
+npm install --legacy-peer-deps
 npm run build
 cd .. 
